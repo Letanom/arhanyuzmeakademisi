1d5750101e5898514b42bfb8c73b6b7bd56ef74e
Letanom <kevinozsimsek@gmail.com>
2025-03-27 14:40:46 +0300

    Update Next.js config to disable image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
+    domains: ['localhost'],
     remotePatterns: [
       {
         protocol: 'https',
